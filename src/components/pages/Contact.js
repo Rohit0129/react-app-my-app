b3e0739f0b0935487b467c886cb64e16b0f663b8
Rohit0129 <53162404+Rohit0129@users.noreply.github.com>
2024-02-26 23:49:38 +0530

    my-app

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -38,9 +38,9 @@ function Contact() {
                         <div className='form-group mt-4'>
                             <button type="button" className='btn-frm shadow w-100'>Send Message</button>                        </div>
                     </div>
-                    <div className='col-md-6'>
-                        <div>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193746.99544999623!2d-74.10980954660558!3d40.64538737078174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25b0a449c39c9%3A0xc62355aa46c985fa!2sBrooklyn%20Museum!5e0!3m2!1sen!2sin!4v1708530694161!5m2!1sen!2sin" width="600" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+                    <div className='col-md-6 pt-4'>
+                        <div className='map-sec'>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193746.99544999623!2d-74.10980954660558!3d40.64538737078174!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25b0a449c39c9%3A0xc62355aa46c985fa!2sBrooklyn%20Museum!5e0!3m2!1sen!2sin!4v1708530694161!5m2!1sen!2sin" width="600" height="450"></iframe>
                         </div>
                     </div>
                 </div>
